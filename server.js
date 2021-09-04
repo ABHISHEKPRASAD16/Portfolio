3670117e992e75bd1686288d2feaef88fc8dfd71
lucifer686 <77689576+lucifer686@users.noreply.github.com>
2021-09-04 12:36:40 +0530

    first deployment

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const { check, validationResult } = require('express-validator');
 
 const app = express();
 
+//connect mangodb
+connectDB();
+
+//Init Middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res)  =>  res.send('API RUNNING'));
+
+//define routes
+
+app.use('/api/users',require('./routes/api/users'));
+
+app.use('/api/auth',require('./routes/api/auth'));
+
+
+app.use('/api/profile',require('./routes/api/profile'));
+
+
+app.use('/api/posts',require('./routes/api/post'));
+
 
 const port = process.env.PORT || 5000;
 
