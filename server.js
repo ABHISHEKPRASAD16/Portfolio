6675b85bdf824bcc893f31fb1b49c233df228478
lucifer686 <77689576+lucifer686@users.noreply.github.com>
2021-09-08 17:30:02 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const { check, validationResult } = require('express-validator');
+const path = require('path')
 
 const app = express();
 
@@ -10,7 +11,7 @@ connectDB();
 //Init Middleware
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res)  =>  res.send('API RUNNING'));
+
 
 //define routes
 
@@ -23,6 +24,16 @@ app.use('/api/profile',require('./routes/api/profile'));
 
 
 app.use('/api/posts',require('./routes/api/post'));
+
+// SERVE STATIC ASSEST AND PRODUCTION
+if(process.env.NODE_ENV === 'production') {
+
+    //Set static folder
+    app.use(express.static(client/build));
+    app.get('*',( req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 
 const port = process.env.PORT || 5000;
